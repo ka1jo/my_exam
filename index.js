1f05965c5f98b051a32ac4e9e313930b764f8038
Пользователь <pitann3@gmail.com>
2022-12-24 20:02:32 +0400

    14

--- index.js
+++ index.js
@@ -21,3 +21,21 @@ function anim()
 
 
 
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+ if(!menuOpen){
+    menuBtn.classList.add("open");
+    menuOpen = true ;
+ } else{
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+ }
+})
+
+
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
